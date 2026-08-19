0de6d17d1ad7cdda363f16ee2ea01e800ff81b45
Dione Pinheiro <dionepinheiro6030@gmail.com>
2026-08-19 18:33:39 -0300

    first commit

---
+++ Atividade.py
@@ -0,0 +1,14 @@
+funcionalidades = {"Ler": True,
+            "Escrever": True,
+            "Apagar": True,
+            "Exportar": True}
+
+def geraCombinacoes(funcionalidades):
+    quantidade = len(funcionalidades)
+    
+
+def dividirTrabalho(totalCombinacoes, quantidadeThreds):
+    a
+
+def processarCombinacoes(inicio, fim):
+    a
